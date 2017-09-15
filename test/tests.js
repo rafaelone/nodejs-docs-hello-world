a61ea0113e2879e5b965d427125b517cf9214edc
logonrm <logonrm@fiap.com.br>
2017-09-15 11:26:41 -0300

    add teste unitario

---
+++ test/tests.js
@@ -0,0 +1,39 @@
+process.env.PORT = '3000';
+var server = require('../index');
+
+describe('server', function () {
+  before(function () {
+    server.listen(3000);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+var assert = require('assert'),
+    http = require('http');
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:3000', function (response) {
+      assert.equal(200, response.statusCode);
+      done();
+    });
+  });
+
+  it('should say Hello World in node.js', function (done) {
+    http.get('http://localhost:3000', function (response) {
+      var data = '';
+
+      response.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      response.on('end', function () {
+        assert.equal('Hello World in node.js', data);
+        done();
+      });
+    });
+  });
+});
